8b1ef40fae09e3d3ea31afa7d60b7efea7985e49
caseyfee <caseyfee3@gmail.com>
2023-05-21 12:52:16 -0700

    updated about and fixed footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faGithub,faSpotify, faLinkedin} from '@fortawesome/free-brands-svg-icons';
+import { faGithub, faSpotify, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 
 
@@ -17,14 +17,19 @@ const FooterComp = () => {
             </ul> */}
           </div>
           <div className="col-md-4">
-            <ul className="list-unstyled">
-            <FontAwesomeIcon icon={faGithub} href="https://github.com/caseyfee" target="_blank" />
-            <FontAwesomeIcon icon={faLinkedin} href = "https://www.linkedin.com/in/cfee/" target="_blank" /> 
+            <ul className="list-unstyled"  >
+              <a href="https://github.com/caseyfee" target="_blank" rel="noreferrer">
+                <FontAwesomeIcon icon={faGithub} />
+              </a>
+              <a href="https://www.linkedin.com/in/cfee/" target="_blank" rel="noreferrer">
+                <FontAwesomeIcon icon={faLinkedin} />
+              </a>
+              <a href="https://open.spotify.com/user/12124142261" target="_blank" rel="noreferrer">
+                <FontAwesomeIcon icon={faSpotify} />
+              </a>
 
-            <FontAwesomeIcon icon={faSpotify} href = "https://open.spotify.com/user/12124142261" target="_blank" />
 
-
-             {/* <li><a href="https://github.com/caseyfee"><i className="fab fa-facebook-f"></i></a></li>
+              {/* <li><a href="https://github.com/caseyfee"><i className="fab fa-facebook-f"></i></a></li>
               <li><a href="https://open.spotify.com/user/12124142261"><i className="fab fa-spotify"></i></a></li>
               <li><a href="https://www.linkedin.com/in/cfee/"><i className="fab fa-linkedin-in"></i></a></li> */}
             </ul>
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -20,20 +20,10 @@ export default function About() {
           src={photo}
           alt="Selfie"
         />
-        Hi there - I am an engineer who loves to learn and just started on the new career path of learning Full Stack Coding methods. My background was in chemical engineering through the lense of environmental compliance for the last 8 years and am excited to pivot to the tech field. In my free time I love reading, dancing, making pottery, and being outside.
-
-        At this moment in my career, I am seeking a role in a medium size company who create something that adds to the greater good of the world. This could take many forms and I'm excited to work with the career center to network and find my next position.
-
-        Don't hesitate to reach out if you have questions.
+        Hey - I'm Casey Fee. I love to learn and am excited to be falling backwards into this new world of coding. Since 2014, when my first INDEX(MATCH) worked in my Excel Database, I've been diving deeper and deeper into what programming can do. Behind this passion is a chemical engineer degree and 8 years of enviornmentally and mission focussed career moves. 
         
-
-        Back in 2012, I decided to try my hand at creating custom Tumblr themes and tumbled head first into the rabbit hole of coding and web development. Fast-forward to today, and I’ve had the privilege of building software for an advertising agency, a start-up, a student-led design studio, and a huge corporation.
-
-        My main focus these days is building products and leading projects for our clients at Upstatement. In my free time I've also released an online video course that covers everything you need to know to build a web app with the Spotify API.
-
-        When I’m not at the computer, I’m usually rock climbing, hanging out with my wife and two cats, or running around Hyrule searching forKorok seeds
-        
-
+        At this moment in my career, I am seeking a role in a medium size company who create something that adds to the greater good of the world. 
+        In my free time you can find me either working with my hands making pottery, carving spoons, moving my body up a moutain or around a dance floor, or completely still in a library book. 
 
       </p>
     </div>
