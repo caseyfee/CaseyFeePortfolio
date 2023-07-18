ee02a27fd7784296983a61fe0545e2e525999aec
caseyfee <caseyfee3@gmail.com>
2023-07-18 12:27:06 -0700

    comment has next steps

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -19,7 +19,8 @@ export default function PortfolioContainer() {
   // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
   const renderPage = () => {
     if (currentPage === 'Resume') {
-      return <window.open("https://drive.google.com/file/d/1jqQ2II3jsyrY5Bq0aKBzfW7u8I3MZ_w3/view?usp=sharing","_blank");
+      return <Resume/>
+      // return <window.open("https://drive.google.com/file/d/1jqQ2II3jsyrY5Bq0aKBzfW7u8I3MZ_w3/view?usp=sharing","_blank");
     }
     if (currentPage === 'Portfolio') {
       return <PortfolioInfo />;
@@ -28,9 +29,12 @@ export default function PortfolioContainer() {
       return <Contact />;
     }
     if (currentPage === 'About') {
-      return <Home />;
+      return <About />;
     }
-    return <Home />;
+
+    return <
+      // add in if statment here with option to take row 42-54 without nav bar
+    Home />;
   };
 
   const handlePageChange = (page) => setCurrentPage(page);
